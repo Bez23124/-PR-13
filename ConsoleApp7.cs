6c123146991dcaf629cc9ba489d6cb165e085a0d
Bez23124 <157889944+Bez23124@users.noreply.github.com>
2024-11-26 11:21:31 +0200

    Add files via upload

---
+++ ConsoleApp7.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {/// <summary>
+     /// Найти s=min(a, b) + min(c, d),
+     /// используя вспомогательные функции (ввода аргументов, нахождения минимальной величины, вывода результата).
+     /// </summary>
+     /// <param name="args"></param>
+        static int Input(string message)
+        {
+                Console.WriteLine(message);
+                return int.Parse(Console.ReadLine());
+            }
+            static int Min(int a, int b)
+            {
+                return a < b ? a : b;
+            }
+            static void Output(int result)
+            {
+                Console.WriteLine($"result = {result}");
+            }
+            static void Main(string[] args)
+            {
+                int a = Input("a=");
+                int b = Input("b=");
+                int s = Input("s=");
+                int c = Input("c=");
+                int result = Min(a, b) + Min(s, c);
+                Output(result);
+                Console.ReadKey();
+            }
+    }
+}
